9cebfa8287a41c675eec4ec05bd1fa8eb47ec4ea
agasthya112002 <97574869+agasthya112002@users.noreply.github.com>
2022-08-21 14:10:11 +0530

    Done

--- [4] Dijkstras Algorithm.java
+++ [4] Dijkstras Algorithm.java
@@ -14,7 +14,7 @@ System.out.println("Enter the cost adjacency matrix");
 a = new int[n][n];
 for (int i=0;i<n;i++)
  {
-  for (int j=0;j<n;j++)
+  for (int j=0;j<n;j++) 
    {
     a[i][j] = in.nextInt();
    }
